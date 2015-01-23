7f609372528bde514c303357b381f13cb9c76243
Tirso Peralta <tirso.peralta@gmail.com>
2015-01-23 10:52:14 -0500

    Exercises 1-18

---
+++ README.md
@@ -0,0 +1,3 @@
+#Learn Ruby The Hard Way - 3rd Edition
+
+Exercises from the online book [Ruby The Hard Way](http://learnrubythehardway.org/book/).
---
+++ exercises/ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!!"
+puts "Hello Again"
+puts "I like tping this."
+#puts "This is fun."
+puts "Yay! Printing."
+puts "I'd rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "Another line."
